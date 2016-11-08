c1dcbea8fa8015ad7f6aa195bb2a59ffc84d81d8
Fernando Torterolo <nandotorterolo@gmail.com>
2016-11-08 18:37:30 -0300

    fix random cabal

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,23 @@
 module Main where
 
 import Data.List (intercalate)
-import Data.Maybe (isNothing)
+import Data.Maybe
 import Control.Arrow ((&&&))
 import System.Random
 
 data Player = PlayerWhite | PlayerBlack                                      deriving (Eq, Show, Enum)
-data Fil = F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8                             deriving (Eq,Ord,Enum)
-data Col = CA | CB | CC | CD | CE | CF | CG | CH                             deriving (Eq,Ord,Enum)
-data Color = Blanca | Negra                                                  deriving (Eq)
-data Unidad = Rey| Mago | Bufo | Arco | Caballo | Pica | Espada | Hacha      deriving (Eq)
-data Coordenada = Coord Fil Col                                              deriving (Eq)
+data GameStatus = Turn Player | Roll Player | Finished                       deriving (Eq, Show)
+-- Tablero, NroTurno,JugadorActivo, dado1,dado2
+data GameState = GameState Tablero Int Player (Int,Int)                      deriving (Eq) --TODO
+data GameAction = DiceRoll Int Int | Move FichaTablero Coordenada | Pasar    deriving (Eq) --TODO
 
-type GameState = (Tablero,Int,Player)
+data Fil = F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8                             deriving (Eq,Ord,Enum, Show)
+data Col = CA | CB | CC | CD | CE | CF | CG | CH                             deriving (Eq,Ord,Enum, Show)
+data Color = Blanca | Negra                                                  deriving (Eq, Show)
+data Unidad = Rey| Mago | Bufo | Arco | Caballo | Pica | Espada | Hacha      deriving (Eq, Show)
+data Coordenada = Coord Fil Col                                              deriving (Eq, Show)
+
+--type GameState = (Tablero,Int,Player)
 type Ficha = (Unidad,Color)
 type FichaTablero = (Unidad,Color,Coordenada)
 type Tablero = [FichaTablero]
@@ -109,8 +114,8 @@ showBoard fichas =
   row rowNumber fichas = "| " ++ rowNumber ++ "  | " ++ intercalate " | " fichas ++ " |\n" ++ emptyRow
 
 showIOBoard :: IO Tablero -> IO String
-showIOBoard fichas = 
-  do 
+showIOBoard fichas =
+  do
     tablero <- fichas
     return (showBoard tablero)
 
@@ -148,46 +153,40 @@ posicionesValidasNegrasLibres :: Tablero -> [Coordenada]
 posicionesValidasNegrasLibres t = [c | c <- posicionesValidasNegras, estaLibre t c]
 
 posicionesValidasNegras :: [Coordenada]
-posicionesValidasNegras = [Coord f c | f <- [F1 .. F3], c<- [CA .. CH], not (elem (Coord f c) posicionesReyNegro)]
+posicionesValidasNegras = [Coord f c | f <- [F1 .. F3], c<- [CA .. CH],  Coord f c `notElem` posicionesRey Negra]
 
-posicionesReyNegro :: [Coordenada]
-posicionesReyNegro = [Coord F1 CD, Coord F1 CE, Coord F2 CD, Coord F2 CE]
+posicionesRey :: Color -> [Coordenada]
+posicionesRey Negra  = [Coord F1 CD, Coord F1 CE, Coord F2 CD, Coord F2 CE]
+posicionesRey Blanca = [Coord F7 CD, Coord F7 CE, Coord F8 CD, Coord F8 CE]
+
+-- posicionesReyNegro :: [Coordenada]
+-- posicionesReyNegro = [Coord F1 CD, Coord F1 CE, Coord F2 CD, Coord F2 CE]
 
 -- Lista de coordenadas validas al inicio del juego para las blancas
 posicionesValidasBlancasLibres :: Tablero -> [Coordenada]
 posicionesValidasBlancasLibres t = [c | c <- posicionesValidasBlancas, estaLibre t c]
 
 posicionesValidasBlancas :: [Coordenada]
-posicionesValidasBlancas = [Coord f c | f <- [F6 .. F8],  c <- [CA .. CH], not (elem (Coord f c) posicionesReyBlanco)]
-
-posicionesReyBlanco :: [Coordenada]
-posicionesReyBlanco = [Coord F7 CD, Coord F7 CE, Coord F8 CD, Coord F8 CE]
+posicionesValidasBlancas = [Coord f c | f <- [F6 .. F8],  c <- [CA .. CH], Coord f c `notElem` posicionesRey Blanca]
 
 -- Utilidades
 obtenerElementoRandomico :: [a] -> IO a
 obtenerElementoRandomico xs = do
-  n <- randomRIO (0, (length xs) - 1)
+  n <- randomRIO (0, length xs - 1)
   return $ xs !! n
 
--- showIOBoard (posicionarFichaRandomico (Rey, Blanca) []) >>= putStr
--- showIOBoard (posicionarFichaRandomico (Hacha, Blanca) []) >>= putStr
 posicionarFichaRandomico :: Ficha -> Tablero -> IO Tablero
-posicionarFichaRandomico (Rey, Blanca) t = 
+posicionarFichaRandomico (Rey, c) t =
   do
-    posicion <- obtenerElementoRandomico posicionesReyBlanco
-    return ((Rey, Blanca, posicion) : t)
+    posicion <- obtenerElementoRandomico (posicionesRey c)
+    return ((Rey, c, posicion) : t)
 
-posicionarFichaRandomico (Rey, Negra) t = 
-  do
-    posicion <- obtenerElementoRandomico posicionesReyNegro
-    return ((Rey, Negra, posicion) : t)
-
-posicionarFichaRandomico (u, Blanca) t = 
+posicionarFichaRandomico (u, Blanca) t =
   do
     posicion <- obtenerElementoRandomico (posicionesValidasBlancasLibres t)
     return ((u, Blanca, posicion) : t)
 
-posicionarFichaRandomico (u, Negra) t = 
+posicionarFichaRandomico (u, Negra) t =
   do
     posicion <- obtenerElementoRandomico (posicionesValidasNegrasLibres t)
     return ((u, Negra, posicion) : t)
@@ -205,3 +204,119 @@ crearTableroCompleto (ficha:xs) t =
   do
     tablero <- posicionarFichaRandomico ficha t
     crearTableroCompleto xs tablero
+
+
+-- nuestas logicas ------------------------------------------------------------------------------------
+estoyEncarga :: (Int,Int) -> Bool
+estoyEncarga (a,b) = a==b
+
+-- data Fil = F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8                             deriving (Eq,Ord,Enum)
+-- data Col = CA | CB | CC | CD | CE | CF | CG | CH                             deriving (Eq,Ord,Enum)
+
+-- show (movimientoDiagonal (Rey, Blanca, Coord F1 CA))
+movimientoDiagonal :: FichaTablero -> [FichaTablero]
+movimientoDiagonal (uni, color, Coord f c) =
+  izqAba ++ izqArr ++ derAba ++ derArr
+  where
+    izqAba = [(uni, color, Coord (pred f) (pred c)) | f > F1, c > CA]
+    izqArr = [(uni, color, Coord (pred f) (succ c)) | f > F1, c < CH]
+    derAba = [(uni, color, Coord (succ f) (pred c)) | f < F8, c > CA]
+    derArr = [(uni, color, Coord (succ f) (succ c)) | f < F8, c < CH]
+
+
+-- show (movimientoCruz (Rey, Blanca, Coord F1 CA))
+movimientoCruz :: FichaTablero -> [FichaTablero]
+movimientoCruz (uni, color, Coord f c) =
+  izq ++ der ++ aba ++ arr
+  where
+    izq = [(uni, color, Coord (f) (pred c)) | c > CA]
+    der = [(uni, color, Coord (f) (succ c)) | c < CH]
+    aba = [(uni, color, Coord (pred f) (c)) | f > F1]
+    arr = [(uni, color, Coord (succ f) (c)) | f < F8]
+
+-- ----------------------------------------------------------------------------------
+
+startState :: (Int, Int) -> GameState
+startState _ = error "startState has not been implemented!" --TODO
+
+status :: GameState -> GameStatus
+status gs = if isFinished gs then Finished else error "no sabemos bien que va aca y no tengo tildes" --TODO
+
+activePlayer :: GameState -> Maybe Player
+activePlayer gs = case status gs of
+  Turn player -> Just player
+  Roll player -> Just player
+  Finished -> Nothing
+
+  -- data GameState = GameState Tablero Int Player (Int,Int)                      deriving (Eq) --TODO
+  -- data GameAction = DiceRoll Int Int | Move FichaTablero Coordenada | Pasar    deriving (Eq) --TODO
+
+actions :: GameState -> Player -> [GameAction]
+actions (GameState tablero _ _ _) player = error "actions has not been implemented!" --TODO
+  -- let fichas = [(uni,col,coord) | (uni,col,coord) <- tablero, col==color]
+      -- map (\f -> movimiento f  ) fichas
+
+-- ficha, primer dado, segundo dado, tablero
+-- estoy en una carga o no?
+
+-- movimiento :: FichaTablero -> (Int,Int) -> Tablero -> [GameAction]
+-- movimiento (uni, col, coord) dados tablero = case estoyEncarga dados of
+  -- True ->
+    -- let puntos = (fst dados) * 2
+  -- False -> []
+
+
+nextState :: GameState -> Player -> GameAction -> GameState
+nextState _ _ _ = error "nextState has not been implemented!" --TODO
+
+isFinished :: GameState -> Bool
+isFinished (GameState _ 30 _ _ ) = True
+isFinished (GameState tablero _ _ _ ) = length [u | (u,col,coor)<-tablero,u==Rey] /= 2 --Si no están los dos reyes el juego terminó
+
+score :: GameState -> Player -> Maybe Int
+score _ _ = error "score has not been implemented!" --TODO
+
+-- Presentation ------------------------------------------------------------------------------------
+
+instance Show GameState where
+   show _ = error "(Show GameState) has not been implemented!" --TODO
+
+instance Show GameAction where
+   show _ = error "(Show GameAction) has not been implemented!" --TODO
+
+instance Read GameAction where
+   readsPrec _ = error "(Read GameAction) has not been implemented!" --TODO
+
+-- Match controller --------------------------------------------------------------------------------
+type Agent = GameState -> IO GameAction
+
+consoleAgent :: Player -> Agent
+consoleAgent _ _ = error "consoleAgent has not been implemented!"
+
+randomAgent :: Player -> Agent
+randomAgent _ _ = error "consoleAgent has not been implemented!"
+
+runMatch :: RandomGen r => (Agent, Agent) -> GameState -> r -> IO (Int, Int)
+runMatch ags@(agWhite, agBlack) g r = do
+   putStrLn (show g)
+   case (status g) of
+      Finished -> return (fromJust (score g PlayerWhite), fromJust (score g PlayerBlack))
+      (Roll p) -> do
+         let ((d1, d2), r2) = roll2Dice r
+         runMatch ags (nextState g p (DiceRoll d1 d2)) r2
+      (Turn p) -> do
+         move <- (if p == PlayerWhite then agWhite else agBlack) g
+         runMatch ags (nextState g p move) r
+
+runOnConsole :: IO (Int, Int)
+runOnConsole = do
+   r <- newStdGen
+   let (dice, r2) = roll2Dice r
+   runMatch (consoleAgent PlayerWhite, consoleAgent PlayerBlack) (startState dice) r2
+
+-- Utility -----------------------------------------------------------------------------------------
+
+roll2Dice :: (RandomGen g) => g -> ((Int, Int), g)
+roll2Dice g = ((v1, v2), g2)
+   where (v1, g1) = randomR (1, 6) g
+         (v2, g2) = randomR (1, 6) g1
